80645da554a8961e5fd638bc13080373d53bc50e
softmoca <acyc0909@gmail.com>
2024-05-25 16:56:00 +0900

    CCDD Test

--- src/main/java/org/spot/pratice/controller/ProfileController.java
+++ src/main/java/org/spot/pratice/controller/ProfileController.java
@@ -14,7 +14,7 @@ public class ProfileController {
     private final Environment env;
     private static final String NULL = "";
 
-    @GetMapping("/profiles")
+    @GetMapping("/profile")
     public String getProfile() {
         return Arrays.stream(env.getActiveProfiles())
                 .findFirst()
